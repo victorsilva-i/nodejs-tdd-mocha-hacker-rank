ee84042d98ba16f26a7c52d5c8f15a5884578943
User <user.push@hackerrank.workspace>
2021-05-15 01:27:33 +0000

    [Admin Commit] save changes
    Commit by HackerRank

--- controllers/reminders.js
+++ controllers/reminders.js
@@ -1,7 +1,20 @@
 const Reminders = require('../models/reminders');
-const {Op} = require('sequelize');
+const { Op } = require('sequelize');
+const Reminder = require("../models/reminders")
 
 
 module.exports = {
+
+  create: (req) => {
+    return new Promise((resolve, reject) => {
+      Reminder.create(req.body)
+        .then(res => {
+          resolve(res)
+        })
+        .catch(e => {
+          reject(e)
+        })
+    })
+  }
 
 }
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,21 @@
 var express = require('express');
 var router = express.Router();
+var reminders = require("../controllers/reminders")
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   res.send('<p>HTML Data</p>');
+});
+
+router.post('/reminders', async function (req, res, next) {
+  try {
+    const reminderCreated = await reminders.create(req);
+    res.status(200).send(reminderCreated)
+  }
+  catch (error) {
+    console.log(error)
+  }
+
 });
 
 module.exports = router;
